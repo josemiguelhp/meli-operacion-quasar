07a463602dd38ad9c0b6c9be46afdfa7b03386e5
josemiguelins <peraestudios@gmail.com>
2021-02-06 00:58:23 -0300

    feature: initial commit

---
+++ src/main/java/com/josehernandez/meli/MeliOperacionQuasarApplication.java
@@ -0,0 +1,13 @@
+package com.josehernandez.meli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliOperacionQuasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeliOperacionQuasarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josehernandez/meli/MeliOperacionQuasarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josehernandez.meli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliOperacionQuasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
